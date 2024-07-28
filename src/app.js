8f825bcbda9677d3503375345db6fb0d49366b69
Ruann Silva <ruansilva6721@gmail.com>
2024-07-28 17:58:40 -0300

    adicionando CORS

--- src/app.js
+++ src/app.js
@@ -3,24 +3,29 @@ import cors from 'cors';
 import router from './routes.js';
 import fs from 'fs';
 import https from 'https';
-
-
+import http from 'http';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
 
+const PORT_HTTP = process.env.PORT_HTTP || 3000;
+const PORT_HTTPS = process.env.PORT_HTTPS || 3001;
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, async () => {
-  console.log(`Api Rodando na porta ${PORT}`);
+// HTTP Server
+http.createServer(app).listen(PORT_HTTP, () => {
+  console.log(`API Rodando na porta ${PORT_HTTP}`);
 });
 
+// HTTPS Server
 if (fs.existsSync('src/SSL/code.crt') && fs.existsSync('src/SSL/code.key')) {
   https.createServer({
     cert: fs.readFileSync('src/SSL/code.crt'),
     key: fs.readFileSync('src/SSL/code.key')
-  }, app).listen(3001, () => console.log("Rodando em https na porta 3001"));
+  }, app).listen(PORT_HTTPS, () => {
+    console.log(`API Rodando em https na porta ${PORT_HTTPS}`);
+  });
+} else {
+  console.log('Certificados SSL não encontrados. HTTPS não será iniciado.');
 }
